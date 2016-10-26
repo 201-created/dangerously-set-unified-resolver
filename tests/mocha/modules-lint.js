3a590b2bf92bce7e51994c97f8cacc4cc606b4e3
Isaac Ezer <isaac.ezer@gmail.com>
2016-10-26 12:20:31 +0200

    use broccoli-fixturify

--- tests/mocha/modules-lint.js
+++ tests/mocha/modules-lint.js
@@ -4,6 +4,7 @@ var expect = require('chai').expect;
 var ModulesLintPlugin = require('../../lib/modules-lint');
 var sinon = require('sinon');
 var broccoli = require('broccoli');
+var Fixturify = require('broccoli-fixturify');
 
 var builder;
 
@@ -20,13 +21,19 @@ describe('modules-lint', function() {
   });
 
   it('works', function() {
-    var sourcePath = 'tests/mocha/fixtures';
-    var node = new ModulesLintPlugin(sourcePath);
+    var desiredDirectory = {
+      'foo.txt': 'foo.txt contents',
+      'subdir': {
+        'bar.txt': 'bar.txt contents'
+      }
+    };
+    var tree = new Fixturify(desiredDirectory);
+    var node = new ModulesLintPlugin(tree);
 
     builder = new broccoli.Builder(node);
     return builder.build().then(function(result) {
       expect(console.log.calledWith('in the plugin')).to.be.ok;
       expect(console.log.calledWith('foo.txt')).to.be.ok;
-  });
+    });
   });
 });
